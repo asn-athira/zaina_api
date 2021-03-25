d9c489ca8b8586fd07ba8f4f05731082ad324d18
asn-athira <athira@rightsolution.ae>
2021-03-25 18:30:12 +0530

    modified

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins 'http://localhost:3000','https://zaina-app.vercel.app/'
+    origins 'https://zaina-app.vercel.app'
   
     resource '*',
       headers: :any,

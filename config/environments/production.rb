76c05966a1316fa0ad997b4e1b6df07e57c45583
asn-athira <athira@rightsolution.ae>
2021-04-02 15:42:12 +0530

    reset pwd fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "zaina_api_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://zaina-api.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey',
+    :password => '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',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
